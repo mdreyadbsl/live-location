1ceaf80c071d0db8be46166b8a8c44bec0fd3fea
mdreyadbsl <mdreyad33633@gmail.com>
2026-08-01 19:02:10 +0600

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -1,1 +1,8 @@
 
+import { database, loginAnonymous } from "./firebase.js";
+import { createMap, updateMarker } from "./map.js";
+import { formatTime, formatAccuracy } from "./utils.js";
+
+console.log("✅ tracker.js loaded");
+
+// পরবর্তী ধাপে এখানেই পুরো tracking logic আসবে।
